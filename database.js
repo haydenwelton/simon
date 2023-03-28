31240ce9e3eda9d0a164ded0eb97434a885cb650
haydenwelton <112789689+haydenwelton@users.noreply.github.com>
2023-03-27 19:17:33 -0600

    Updates to simon

---
+++ database.js
@@ -0,0 +1,34 @@
+const {MongoClient} = require('mongodb');
+
+const userName = process.env.MONGOUSER;
+const password = process.env.MONGOPASSWORD;
+const hostname = process.env.MONGOHOSTNAME;
+console.log(userName);
+console.log(password);
+console.log(hostname);
+console.log(process.env);
+
+if (!userName) {
+  throw Error('Database not configured. Set environment variables');
+}
+
+const url = `mongodb+srv://haywelton:9l7UxJPWvpRzRPdu@myservice.oqtkrzk.mongodb.net/?`;
+
+const client = new MongoClient(url);
+const scoreCollection = client.db('simon').collection('score');
+
+function addScore(score) {
+  scoreCollection.insertOne(score);
+}
+
+function getHighScores() {
+  const query = {score: {$gt: 0}};
+  const options = {
+    sort: {score: -1},
+    limit: 10,
+  };
+  const cursor = scoreCollection.find(query, options);
+  return cursor.toArray();
+}
+``
+module.exports = {addScore, getHighScores};
